7d56ab936debc78adf8e630483887d5665aedb35
Alex A <alex.av936@gmail.com>
2024-03-19 00:29:04 -0500

    initial and final commit

---
+++ src/app/components/top-navigation/top-navigation.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Top-Navigation',
+  standalone: true,
+  imports: [],
+  templateUrl: './top-navigation.component.html',
+  styleUrl: './top-navigation.component.css'
+})
+export class TopNavigationComponent {
+
+}
